be5d5d39e19ef538335bab6f0cd727d003343a73
Filip Bachul <filipbachul@gmail.com>
2025-11-06 16:27:43 +0100

    chore: remove unnecessary code form frontend

--- apps/frontend/src/XTBParser/cashOperationHistory/parseQuantity.test.ts
+++ apps/frontend/src/XTBParser/cashOperationHistory/parseQuantity.test.ts
@@ -19,11 +19,3 @@ describe("parseQuantity", () => {
     });
   }
 });
-
-const testCasesV2 = [
-  { input: "OPEN BUY 100 @ 9.895", expected: "100" },
-  { input: "OPEN BUY 14/120 @ 62.34", expected: "14" },
-  { input: "CLOSE BUY 10 @ 46.80", expected: "10" },
-  { input: "OPEN BUY 0.0736/4.0736 @ 61.370", expected: "0.0736" },
-  { input: "CLOSE BUY 0.1995/61 @ 16.40", expected: "0.1995" },
-];
