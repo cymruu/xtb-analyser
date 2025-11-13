0ded22ca8fe4146e03618c7d2117d563015b19b1
Filip Bachul <filipbachul@gmail.com>
2025-11-13 20:54:55 +0100

    chore: wip render deposits

--- apps/frontend/src/modules/portfolio-value/drawChart.ts
+++ apps/frontend/src/modules/portfolio-value/drawChart.ts
@@ -2,7 +2,7 @@ import * as Plot from "@observablehq/plot";
 import { RenderContext, Renderer } from "./renderer";
 
 import data from "./response.json";
-import { eachDayOfInterval, subDays } from "date-fns";
+import { eachDayOfInterval, subDays, eachMonthOfInterval } from "date-fns";
 
 export function drawChart(
   renderContext: RenderContext,
@@ -21,9 +21,32 @@ export function drawChart(
     return { date, value: data[i].value };
   });
 
-  console.log(mappedData);
+  const deposits = eachMonthOfInterval({
+    start: subDays(new Date(), data.length - 1),
+    end: new Date(),
+  }).map((date, i) => {
+    return { date, value: i * 1000 };
+  });
 
-  const plot = Plot.areaY(mappedData, { x: "date", y: "value" }).plot();
+  console.log({ mappedData, deposits });
+
+  const plot = Plot.plot({
+    y: {
+      grid: true,
+    },
+    marks: [
+      Plot.areaY(mappedData, { x: "date", y: "value", fillOpacity: 0.3 }),
+      Plot.lineY(mappedData, { x: "date", y: "value" }),
+      Plot.rectY(deposits, {
+        x: "date",
+        y: "value",
+        interval: "week",
+      }),
+
+      // Plot.crosshairX(mappedData, { x: "date", y: "value" }),
+      Plot.ruleY([0]),
+    ],
+  });
 
   return plot;
 }
