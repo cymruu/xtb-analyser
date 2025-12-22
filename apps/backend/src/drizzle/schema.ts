4c3dd6b144981bb338dfdf2a3c52b09ed4775738
Filip Bachul <filipbachul@gmail.com>
2025-12-22 11:09:31 +0100

    chore: remove prisma table

--- apps/backend/src/drizzle/schema.ts
+++ apps/backend/src/drizzle/schema.ts
@@ -1,18 +1,6 @@
-import { mysqlTable, mysqlSchema, AnyMySqlColumn, primaryKey, unique, int, varchar, datetime, json, double, text } from "drizzle-orm/mysql-core"
-import { sql } from "drizzle-orm"
+import { sql } from "drizzle-orm";
+import { datetime, double, int, mysqlTable, primaryKey, unique, varchar } from "drizzle-orm/mysql-core";
 
-export const portfolio = mysqlTable("Portfolio", {
-  id: int().autoincrement().notNull(),
-  uuid: varchar({ length: 191 }).notNull(),
-  createdAt: datetime({ mode: 'string', fsp: 3 }).default(sql`(CURRENT_TIMESTAMP(3))`).notNull(),
-  updatedAt: datetime({ mode: 'string', fsp: 3 }).notNull(),
-  schemaVersion: varchar({ length: 191 }).notNull(),
-  schema: json().notNull(),
-},
-  (table) => [
-    primaryKey({ columns: [table.id], name: "Portfolio_id" }),
-    unique("Portfolio_uuid_key").on(table.uuid),
-  ]);
 
 export const yahooPrice = mysqlTable("YahooPrice", {
   id: int().autoincrement().notNull(),
@@ -32,16 +20,4 @@ export const yahooPrice = mysqlTable("YahooPrice", {
     unique("YahooPrice_symbol_datetime_key").on(table.symbol, table.datetime),
   ]);
 
-export const prismaMigrations = mysqlTable("_prisma_migrations", {
-  id: varchar({ length: 36 }).notNull(),
-  checksum: varchar({ length: 64 }).notNull(),
-  finishedAt: datetime("finished_at", { mode: 'string', fsp: 3 }),
-  migrationName: varchar("migration_name", { length: 255 }).notNull(),
-  logs: text(),
-  rolledBackAt: datetime("rolled_back_at", { mode: 'string', fsp: 3 }),
-  startedAt: datetime("started_at", { mode: 'string', fsp: 3 }).default(sql`(CURRENT_TIMESTAMP(3))`).notNull(),
-  appliedStepsCount: int("applied_steps_count", { unsigned: true }).default(0).notNull(),
-},
-  (table) => [
-    primaryKey({ columns: [table.id], name: "_prisma_migrations_id" }),
-  ]);
+
