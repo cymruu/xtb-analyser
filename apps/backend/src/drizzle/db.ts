771a6ef2962c49dcbc28bd4a69065069dda19ab9
Filip Bachul <filipbachul@gmail.com>
2025-12-22 13:46:36 +0100

    chore: drizzle

--- apps/backend/src/drizzle/db.ts
+++ apps/backend/src/drizzle/db.ts
@@ -1,8 +1,4 @@
 import { drizzle } from 'drizzle-orm/mysql2';
-import mysql from "mysql2/promise";
 
-const connection = await mysql.createConnection({
-  uri: process.env.DATABASE_URL!
-});
+export const db = drizzle({ connection: { uri: process.env.DATABASE_URL! } });
 
-export const db = drizzle({ 'client': connection });
